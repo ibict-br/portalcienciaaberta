a958723036f73c58ca7a33222c7aab9f4c835cb6
Marcos Antônio Vieira Faria <marcosfaria@IN.IBICT.BR>
2022-08-24 16:16:32 -0300

    Barra Brasil Comentada

--- footer.php
+++ footer.php
@@ -1,5 +1,7 @@
-<?php wp_footer(); ?> <!-- Carrega todos os scripts e styles -->
-<footer></footer>
-<script defer="defer" src="//barra.brasil.gov.br/barra_2.0.js" type="text/javascript"></script>
+<?php wp_footer(); ?>
+<!-- Carrega todos os scripts e styles -->
+<!-- <footer></footer> -->
+<!-- <script defer="defer" src="//barra.brasil.gov.br/barra_2.0.js" type="text/javascript"></script> -->
 </body>
+
 </html>
